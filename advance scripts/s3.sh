726a61b77ba5fa9bbfaf5048e59ba1eb2dc9c4cb
parth-18-bansal <parthbansalcoding@gmail.com>
2025-04-14 13:50:33 +0530

    mpstat

--- advance scripts/s3.sh
+++ advance scripts/s3.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
 # Monitor CPU usage and alert if it exceeds a limit
+
+THRESHOLD=80
+
+INTERVAL=60
+
+get_cpu_usage(){
+    usage=$(mpstat 1 1 | awk '/Average:/ {print 100 - $NF}')
+    echo ${usage%.*}
+}
+
+while true; do
+    cpu_usage=$(get_cpu_usage)
+    
+    if [ "$cpu_usage" -gt "$THRESHOLD" ]; then
+        echo "CPU usage is high: ${cpu_usage}%"
+
+    else
+        echo "CPU usage is OK"
+    fi
+
+    sleep $INTERVAL
+done
+
+
+
+
+# mpstat = multiprocessor statistics, it gives the time of cpu and it cores spent where.
