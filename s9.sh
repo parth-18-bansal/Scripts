04f6e13eac85dc4821219ba87e79285e546f32a6
parth-18-bansal <parthbansalcoding@gmail.com>
2025-04-24 00:58:55 +0530

    -w flag in grep

--- s9.sh
+++ s9.sh
@@ -5,3 +5,4 @@
 grep -Eo '\b[a-zA-Z]{5}\b' ./files/f9.txt
 
 # \b = word boundary, it is word boundary between a word and a non-word character
+# we can also use the -w flag for defining that pattern is a single complete word
