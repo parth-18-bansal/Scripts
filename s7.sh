6ad8631484cdeed03e9961b9f8e55d5248d56dad
parth-18-bansal <parthbansalcoding@gmail.com>
2025-06-19 22:43:25 +0530

    new things about awk

--- s7.sh
+++ s7.sh
@@ -6,3 +6,4 @@ awk -F, '{print $2}' ./files/f7.csv
 
 # -F = field separator
 # {} = action that to perform in each line
+# NR = line number
--- s8.sh
+++ s8.sh
@@ -2,9 +2,11 @@
 
 # Print the number of columns in a CSV file
 
-awk -F, '{print NF; exit}' ./files/f7.csv
+awk -F, '{print NF; exit}' ./files/f7.csv  # action
 
-awk -F, '$(NF-1) > 25' ./files/f7.csv
+awk -F, '$(NF-1) > 25' ./files/f7.csv    # condition
+
+awk -F, 'NR > 1 && $(NF-1) > 25' ./files/f7.csv # apply more than two condition.
 
 # NF = number of the field in a line
 # $NF = last field
@@ -12,3 +14,13 @@ awk -F, '$(NF-1) > 25' ./files/f7.csv
 # exit = stop the program after the first line
 # $(NF-1) = second to last field
 # '' = okay but "" then first expression is evaluated by the shell not by awk
+
+
+# {} = used for the custom action
+# '' = condition
+# if we not specify {} then it will use the default action print $0 = print complete line
+
+# there are three ways of the awk
+# 1. condition
+# 2. action
+# 3. condition action
