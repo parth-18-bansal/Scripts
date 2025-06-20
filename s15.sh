bda9da18ac57af22b28c23f9840228805f37de73
parth-18-bansal <parthbansalcoding@gmail.com>
2025-06-20 12:40:09 +0530

    adding more concepts

--- s15.sh
+++ s15.sh
@@ -2,4 +2,7 @@
 
 # Find all words in a text file that start with "s" 
 
-grep -Eo '\bs[a-zA-Z]*\b' ./files/f15.txt
+grep -Eo '(\bs[a-zA-Z]*\b|\bS[a-zA-Z]*\b)' ./files/f15.txt
+grep -Eoi '\bs[a-zA-Z]*\b' ./files/f15.txt
+
+tr -s '[:space:][:punct:]' '\n < ./files/f15.txt | grep -Ei '^s'
