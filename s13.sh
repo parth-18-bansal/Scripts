27ad4618b9fd597585d3e0d62150d210a839129f
parth-18-bansal <parthbansalcoding@gmail.com>
2025-06-20 12:05:49 +0530

    improving the concepts

--- s13.sh
+++ s13.sh
@@ -2,7 +2,12 @@
 
 # Find unique words in a file and sort them alphabetically 
 
-awk '{for(i=1;i<=NF;i++) {print $i}}' ./files/f13.txt | sort -u
+awk '{
+    // action
+    for(i=1;i<=NF;i++){
+        print $i
+    }
+}' ./files/f13.txt | sort -u
 
 grep -Eo '\b[a-zA-Z]+\b' ./files/f13.txt | sort -u
 
