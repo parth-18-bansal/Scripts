ead0c490c5e54d13a782ffcae3205465d1d0af02
parth-18-bansal <parthbansalcoding@gmail.com>
2025-07-04 14:45:52 +0530

    awk concept

--- s28.sh
+++ s28.sh
@@ -2,6 +2,11 @@
 
 # Print every alternate line from a file 
 
-awk 'NR % 2 == 1 {print $0}' ./files/f25.txt
+awk '(NR%2==0){
+
+    print
+
+}' ./files/f25.txt
 
 # NR = number of records, so it give the line number
+# '(condition) {action --> this execute if condition is true} {END action}'
