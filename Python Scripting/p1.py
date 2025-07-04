726aaacf12b2d849c8c405f824f2b0015174cef7
parth-18-bansal <parthbansalcoding@gmail.com>
2025-07-04 15:27:33 +0530

    p1.py

---
+++ Python Scripting/p1.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+# this creates the Path object, after that we can do all the path realted operations
+directory = Path("./")
+
+# listing of the files and folders in that path
+for f in directory.iterdir():
+    print(f)
+
+# to chech whether that directory is exist or not
+print(directory.exists())
+
+#List only files
+files = [f.name for f in directory.iterdir() if f.is_file()]
+print(files)
