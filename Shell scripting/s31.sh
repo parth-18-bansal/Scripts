1cf86fdc05e37e9d553400ee53acf7aaad57d34f
parth-18-bansal <parthbansalcoding@gmail.com>
2025-07-07 14:57:35 +0530

    find

--- Shell scripting/s31.sh
+++ Shell scripting/s31.sh
@@ -2,6 +2,19 @@
 
 # Count the number of files in a directory
 
+# this will list files and directories both
 ls -1 | wc -l
 
+# this list only files
+find . -maxdepth 1 -type f | wc -l
+
+# alternative way to list files
+for file in *; do
+  if [ -f "$file" ]; then
+    echo "$file"
+  fi
+done
+
 # -1 = it will list the files in one column
+# find = search tool for files and directories based on the name, size, modified time, type etc.
+# max depth = to define travel sub folder or not
